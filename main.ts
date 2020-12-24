cc8f54e9a0414c6140928334b2e5c0c586ba2dd2
bryan weinstein <bryanweinstein@outlook.com>
2020-12-24 10:19:44 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,10 +15,10 @@ let PMSentSignal = 0
 let AMSentSignal = 0
 radio.setGroup(1)
 basic.forever(function () {
-    if (input.lightLevel() >= 150 && AMSentSignal == 0) {
+    if (input.lightLevel() >= 70 && AMSentSignal == 0) {
         Sender(0)
     }
-    if (input.lightLevel() <= 70 && PMSentSignal == 0) {
+    if (input.lightLevel() <= 10 && PMSentSignal == 0) {
         Sender(1)
     }
 })
